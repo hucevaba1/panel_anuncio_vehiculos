f52f1a3a9d69f5bb39f6126326d3047db03afe56
Hucevayba <116099241+hucevaba1@users.noreply.github.com>
2026-01-27 13:25:37 -0600

    Use checkboxes and improve scatter plot

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ st.header("Panel de anuncios de vehículos 🚗")
 car_data = pd.read_csv("vehicles_us.csv")
 
 st.write("Vista rápida del dataset:")
-st.dataframe(car_data.head(20))
+st.dataframe(car_data.sample(20))
 
-hist_button = st.button("Construir histograma (odómetro) 🛣️")
+show_hist = st.checkbox("Construir histograma (odómetro) 🛣️")
 
-if hist_button:
+if show_hist:
     st.write("Creación de un histograma para la columna odómetro")
     fig = px.histogram(car_data, x="odometer",  title="Odómetro")
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button(
+show_scatter = st.checkbox(
     "Construir diagrama de dispersión (precio vs odómetro) 📊")
 
-if scatter_button:
+if show_scatter:
     st.write("Creación de un diagrama de dispersión para precio vs odómetro")
     fig = px.scatter(car_data, x="odometer", y="price", color="fuel",
                      title="Precio vs Odómetro por Tipo de Combustible")
